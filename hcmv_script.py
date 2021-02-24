be036b138db077d8684af83020ceeae037c27df1
Jessie Chen <jessiechen0513@gmail.com>
2021-02-24 09:19:43 -0600

    added retrieving code

---
+++ hcmv_script.py
@@ -0,0 +1,16 @@
+import os
+#retrieving HCMV transcriptomes of patients
+#Donor 1 (2dpi)
+os.system("wget https://sra-downloadb.be-md.ncbi.nlm.nih.gov/sos2/sra-pub-run-11/SRR5660030/SRR5660030.1")
+#Donor 1 (6dpi)
+os.system("wget https://sra-downloadb.be-md.ncbi.nlm.nih.gov/sos2/sra-pub-run-11/SRR5660033/SRR5660033.1")
+#Donor 3 (2dpi)
+os.system("wget https://sra-downloadb.be-md.ncbi.nlm.nih.gov/sos2/sra-pub-run-11/SRR5660044/SRR5660044.1")
+#Donor 3 (6dpi)
+os.system("wget https://sra-downloadb.be-md.ncbi.nlm.nih.gov/sos2/sra-pub-run-11/SRR5660045/SRR5660045.1")
+
+#convert to paired-end fastq files
+os.system("fastq-dump -I --split-files SRR5660030.1")
+os.system("fastq-dump -I --split-files SRR5660033.1")
+os.system("fastq-dump -I --split-files SRR5660044.1")
+os.system("fastq-dump -I --split-files SRR5660045.1")
