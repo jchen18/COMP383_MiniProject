8c61b9203232571eeda514cd7cb1ce01eb5bbed2
Jessie Chen <jessiechen0513@gmail.com>
2021-02-27 12:39:05 -0600

    moved genbank code to separate python script

---
+++ genbank_parse.py
@@ -0,0 +1,15 @@
+#accessing the HCMV Genbank genome (NCBI accession EF999921)
+import logging
+from Bio import Entrez
+from Bio import SeqIO
+Entrez.email = "jessiechen0513@gmail.com"
+handle = Entrez.efetch(db="nucleotide", id="EF999921", rettype="gb", retmode="text") #retrieving in Ge>
+record = SeqIO.read(handle, "genbank")
+handle.close()
+
+logging.basicConfig(filename="miniProject.log", level=logging.INFO) #opening file for logging info
+num_CDS = 0
+for feature in record.features: #loop all features and counting ones that are CDS features
+        if feature.type  == "CDS":
+                num_CDS += 1
+logging.info('The HCMV genome (EF999921) has ' + str(num_CDS) + ' CDS.') #write to log file
--- hcmv_script.py
+++ hcmv_script.py
@@ -19,17 +19,5 @@ os.system("fastq-dump -I --split-files SRR5660033.1")
 os.system("fastq-dump -I --split-files SRR5660044.1")
 os.system("fastq-dump -I --split-files SRR5660045.1")
 
-#accessing the HCMV Genbank genome (NCBI accession EF999921)
-from Bio import Entrez
-from Bio import SeqIO
-Entrez.email = "jessiechen0513@gmail.com"
-handle = Entrez.efetch(db="nucleotide", id="EF999921", rettype="gb", retmode="text") #retrieving in Genbank format
-record = SeqIO.read(handle, "genbank")
-handle.close()
-
-logging.basicConfig(filename="miniProject.log", level=logging.INFO) #opening file for logging info
-num_CDS = 0
-for feature in record.features: #loop all features and counting ones that are CDS features
-        if feature.type  == "CDS":
-                num_CDS += 1
-logging.info('The HCMV genome (EF999921) has ' + str(num_CDS) + ' CDS.') #write to log file
+#run thru script that parses through NCBI HCMV genome Genbank file
+os.system("python3 genbank_parse.py")
