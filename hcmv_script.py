2b5eecde6874795d32e078f032043e30bb31dccd
Jessie Chen <jessiechen0513@gmail.com>
2021-02-27 12:05:21 -0600

    added reference transcriptome from NCBI

--- hcmv_script.py
+++ hcmv_script.py
@@ -1,4 +1,8 @@
 import os
+#creating outfile folder
+os.system("mkdir miniProject_Jessie_Chen")
+os.chdir("miniProject_Jessie_Chen")
+
 #retrieving HCMV transcriptomes of patients
 #Donor 1 (2dpi)
 os.system("wget https://sra-downloadb.be-md.ncbi.nlm.nih.gov/sos2/sra-pub-run-11/SRR5660030/SRR5660030.1")
@@ -14,3 +18,18 @@ os.system("fastq-dump -I --split-files SRR5660030.1")
 os.system("fastq-dump -I --split-files SRR5660033.1")
 os.system("fastq-dump -I --split-files SRR5660044.1")
 os.system("fastq-dump -I --split-files SRR5660045.1")
+
+#accessing the HCMV Genbank ref genome (NCBI accession EF999921)
+from Bio import Entrez
+from Bio import SeqIO
+Entrez.email = "jessiechen0513@gmail.com"
+handle = Entrez.efetch(db="nucleotide", id="EF999921", rettype="gb", retmode="text") #retrieving in Genbank format
+record = SeqIO.read(handle, "genbank")
+handle.close()
+
+logging.basicConfig(filename="miniProject.log", level=logging.INFO) #opening file for logging info
+num_CDS = 0
+for feature in record.features: #loop all features and counting ones that are CDS features
+        if feature.type  == "CDS":
+                num_CDS += 1
+logging.info('The HCMV genome (EF999921) has ' + str(num_CDS) + ' CDS.') #write to log file
